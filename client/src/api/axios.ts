88bf6429adf9da251377fc856d115e31b62b3e7a
memories-quy-2002 <manhphuquynguyen@gmail.com>
2024-09-07 16:43:09 +0700

    test

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 declare module "axios" {
-	export interface AxiosRequestConfig {
-		handlerEnabled?: boolean;
-	}
+    export interface AxiosRequestConfig {
+        handlerEnabled?: boolean;
+    }
 }
 
 // Create an Axios instance with the required configuration
 export default axios.create({
-	baseURL: "http://localhost:4000", // Your server's base URL
-	withCredentials: true, // This is crucial for sending cookies in cross-origin requests
-	handlerEnabled: true, // This is optional and can be set or overridden in specific requests
+    baseURL: "https://e-commerce-website-1-svht.vercel.app/", // Your server's base URL
+    withCredentials: true, // This is crucial for sending cookies in cross-origin requests
+    handlerEnabled: true, // This is optional and can be set or overridden in specific requests
 });
